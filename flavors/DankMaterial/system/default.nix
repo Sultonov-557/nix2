aafe350ae6bd31176353026fb5db9950ae3d07b8
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-11-28 10:36:29 +0500

    idk

--- flavors/DankMaterial/system/default.nix
+++ flavors/DankMaterial/system/default.nix
@@ -1,11 +1,7 @@
 { ... }:
 
 {
-  # System-level configuration for DankMaterial flavor
-  # Imports all system modules
-
   imports = [
-    ../../../modules/system/hyprland.nix
     ./stylix.nix
     ./desktop.nix
     ./services.nix
