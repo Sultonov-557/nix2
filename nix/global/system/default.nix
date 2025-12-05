f697078dced2e8893385db0213eadeec3b135a79
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-12-05 14:49:30 +0500

    fix

--- nix/global/system/default.nix
+++ nix/global/system/default.nix
@@ -1,17 +1,23 @@
 { pkgs, ... }:
 
 {
-  imports = [ ./boot.nix ./locale.nix ./network.nix ./nix.nix ./audio.nix ];
+  imports = [
+    ./boot.nix
+    ./locale.nix
+    ./network.nix
+    ./nix.nix
+    ./audio.nix
+  ];
 
   # User Configuration
   users.users.sultonov = {
     isNormalUser = true;
     description = "sultonov";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
     ignoreShellProgramCheck = true;
     shell = pkgs.zsh;
   };
-
-  # System State Version
-  system.stateVersion = "25.11";
 }
