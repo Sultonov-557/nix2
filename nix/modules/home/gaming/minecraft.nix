6925fd744a14af6239a9017c37559c6c23d44974
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-12-07 19:01:49 +0500

    fix minecraft

--- nix/modules/home/gaming/minecraft.nix
+++ nix/modules/home/gaming/minecraft.nix
@@ -1,13 +1,11 @@
-{ inputs, pkgs, ... }:
-{
+{ inputs, pkgs, ... }: {
   imports = [ inputs.xmcl.homeModules.xmcl ];
 
   programs.xmcl = {
     enable = true;
     commandLineArgs = [ ''--password-store="gnome-libsecret"'' ];
-    jres = [
-      pkgs.jre8
-      pkgs.temurin-jre-bin-17
-    ];
+    jres = [ pkgs.jre8 pkgs.temurin-jre-bin-17 ];
   };
+
+  programs.nix-ld.libraries = [ pkgs.jre8 pkgs.temurin-jre-bin-17 ];
 }
